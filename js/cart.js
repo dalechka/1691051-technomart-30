25e5ed988afbcde45f1f27fb96ea7178c0955483
dalechka <76557352+dalechka@users.noreply.github.com>
2021-03-16 02:02:58 +0500

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,15 @@
-const cartLink = document.querySelector(".catalog-button-buy");
+const cartButtons = document.querySelectorAll(".catalog-button-buy");
 const cartPopup = document.querySelector(".modal-cart");
 const cartClose = cartPopup.querySelector(".modal-close");
 
-cartLink.addEventListener("click", function (evt) {
+for (cartButton of cartButtons) {
+  cartButton.addEventListener ("click", function (evt) {
     evt.preventDefault();
     cartPopup.classList.add("modal-display");
-  });
-  
-  cartClose.addEventListener("click", function (evt) {
+  })
+};
+
+cartClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     cartPopup.classList.remove("modal-display");
   });
